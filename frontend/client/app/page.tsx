7f0aeafb08a5cfa4b22240810276252bf8032a99
Deva <writetodevnarayan@gmail.com>
2024-11-15 11:14:50 +0530

    pair with frontend

---
+++ frontend/client/app/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React ,{useEffect,useState}from 'react'
+
+function page() {
+const [message,setMessage]=useState("Loading")
+
+
+useEffect(()=>{
+  fetch("http://localhost:3000").then(
+    response=>response.json()
+  ).then(
+    data=>{
+      console.log(data)
+      setMessage(data.message)
+    }
+  )
+},[])
+
+  return (
+    <div>{message}</div>
+  )
+}
+
+export default page
